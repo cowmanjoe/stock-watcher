7e9968d7b459ab08011bc1263317d8c12087361a
Joshua Yellowley <Joshuayellowley@gmail.com>
2020-04-25 11:15:22 -0700

    Commiting to pull

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -8,27 +8,38 @@ export default class Results extends React.Component {
       id: "",
       sellers: [],
     };
+
+    this.renderSellerCards = this.renderSellerCards.bind(this);
   }
 
-  componentDidMount() {
+  componentDidUpdate() {
     const { item } = this.props.match.params;
     this.setState({ id: item });
+    console.log("hello fetching");
 
-    this.setState({
-      sellers: fetch('localhost:8000/sellers')
-        .then((response) => {
-          response.json();
-        }).then(jsonResponse => {
-          return jsonResponse.results;
-        })
-    });
-
+    fetch('http://localhost:8000/sellers/')
+        .then((response) => response.json())
+        .then(jsonResponse => {
+          console.log(jsonResponse);
+          this.setState({sellers: jsonResponse.results});
+        });
+    
   }
+
+  renderSellerCards() {
+    let list = [];
+    for (let i = 0; i < this.state.sellers.length; i++) {
+      list.push(<li>sellers[i].name</li>)
+    }
+    return <ul>{this.list}</ul>;
+  }
+
+
   render() {
     return (
       <div>
         <h1>{`Searching for ${this.state.id}`}</h1>
-        {this.state.sellers.forEach((seller) => <div>{seller.name}</div>)}
+          {this.renderSellerCards()}
       </div>
     );
   }
