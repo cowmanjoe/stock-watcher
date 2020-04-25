658155591104a97082f6b66e1eeb042766c403dd
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2020-04-25 12:16:44 -0700

    its fucked up

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,46 +1,64 @@
 import React from "react";
-
+import SellerCard from "./SellerCard";
 export default class Results extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       id: "",
-      sellers: ["fug", "frig"],
+      sellers: {},
     };
 
     this.renderSellers = this.renderSellers.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const { item } = this.props.match.params;
     this.setState({ id: item });
 
-    // this.setState({
-    //   sellers:
-    //     fetch("http://localhost:8000/sellers/", {
-    //       headers: { "Content-Type": "application/json" },
-    //     })
-    //       .then((response) => {
-    //         response.json();
-    //       })
-    //       .then((jsonResponse) => {
-    //         return jsonResponse.results.map((seller) => {
-    //           return {
-    //             name: seller.name,
-    //             address: seller.address,
-    //             city: seller.address,
-    //           };
-    //         });
-    //       })
-    //       .catch((error) => console.log(error)),
-    // });
+    let response = await fetch("http://localhost:8000/sellers/", {
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        response.json();
+      })
+      .then((jsonResponse) => {
+        return jsonResponse.results.map((seller) => {
+          return {
+            name: seller.name,
+            address: seller.address,
+            city: seller.address,
+          };
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        return {
+          name: "Error",
+          products: [
+            {
+              name: "Error",
+              stock: "Out of Stock",
+            },
+          ],
+        };
+      });
+    this.setState({
+      sellers: response,
+    });
   }
 
   renderSellers() {
     var list = [];
-    this.state.sellers.forEach((seller, i) => {
-      list.push(<li key={i}>{seller}</li>);
-    });
+
+    if (this.state.sellers) {
+      Object.keys(this.state.sellers).forEach((seller, i) => {
+        list.push(
+          <li key={i}>
+            <SellerCard name={seller.name} products={seller.products} />
+          </li>
+        );
+      });
+    }
 
     return <ul style={{ listStyleType: "none" }}>{list}</ul>;
   }
---
+++ frontend/src/components/SellerCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+export default class SellerCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderProducts = this.renderProducts.bind(this);
+  }
+
+  renderProducts() {
+    let list = [];
+    if (this.props.products) {
+      this.props.products.forEach((product, i) => {
+        list.push(<li key={i}>{product.name + ": " + product.stock}</li>);
+      });
+    }
+
+    return <ul style={{ listStyleType: "none" }}>{list}</ul>;
+  }
+  render() {
+    return (
+      <Paper elevation={3} variant="outlined">
+        <div className="storeName">{this.props.name}</div>
+        <div className="productList">{this.renderProducts()}</div>
+      </Paper>
+    );
+  }
+}
