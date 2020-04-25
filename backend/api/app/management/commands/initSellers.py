79846d5563181c9a196873ceb46b68fcf272ae03
Cowan <ctcumming@gmail.com>
2020-04-25 02:20:35 -0700

    Improved model.

--- backend/api/app/management/commands/initSellers.py
+++ backend/api/app/management/commands/initSellers.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django.core.management.base import BaseCommand, CommandError
-from api.app.models import Seller as Seller
+from api.app.models import Seller as Seller, InventoryReport, Product
+
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
@@ -8,23 +11,28 @@ class Command(BaseCommand):
         parser.add_argument('seller_names', nargs='+', type=str)
 
     def handle(self, *args, **options):
-
-
         # Initialize Dummy Store Data
         for seller_name in options["seller_names"]:
-            if(seller_name == "RANDOM"):
+            if seller_name == "RANDOM":
                 for i in range(10):
                     store_name = 'store' + str(i)
 
-                    if(i % 2 == 0):
+                    if i % 2 == 0:
                         location_name = 'near'
                         stock_name = 'We got Hella'
                     else:
                         location_name = 'far'
                         stock_name = 'We got None'
                     
-                    seller = Seller(name=store_name, location=location_name, stock_levels=stock_name)
+                    seller = Seller(name=store_name, location=location_name)
                     seller.save()
+
+                    product = Product(name="Charmin Ultra", product_type="Toilet Paper")
+                    product.save()
+
+                    inventory_report = InventoryReport(seller=seller, timestamp=datetime.datetime.now(), product=product, level="low")
+                    inventory_report.save()
+
                 print("Initialized Dummy Data")
                 break
 
--- backend/api/app/models.py
+++ backend/api/app/models.py
@@ -5,4 +5,16 @@ class Seller(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    stock_levels = models.CharField(max_length=20)
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    product_type = models.CharField(max_length=100)
+
+
+class InventoryReport(models.Model):
+    timestamp = models.DateTimeField()
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    seller = models.ForeignKey('Seller', on_delete=models.CASCADE, related_name='inventory_reports')
+    level = models.CharField(max_length=20)
