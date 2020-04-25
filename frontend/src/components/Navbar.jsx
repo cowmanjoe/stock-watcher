a7c6d19dcb814856b301bbdc0d9d2411f6948767
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2020-04-25 00:58:52 -0700

    pushing for jashy

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,11 +5,14 @@ import Tab from "@material-ui/core/Tab";
 export default class Navbar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: 1,
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event, newVal) {
-    this.props.changeValue(newVal);
+  handleChange(event, newValue) {
+    this.setState({ value: newValue });
   }
 
   render() {
@@ -17,12 +20,12 @@ export default class Navbar extends React.Component {
       <div>
         <AppBar position="static">
           <Tabs
-            value={this.props.value}
+            value={this.state.value}
             variant="fullWidth"
             onChange={this.handleChange}>
-            <Tab label="Search By Item" />
-            <Tab disabled label="Search By Store" />
-            <Tab label="Sellers" />
+            <Tab value={1} label="Search By Item" />
+            <Tab value={2} disabled label="Search By Store" />
+            <Tab value={3} label="Sellers" />
           </Tabs>
         </AppBar>
       </div>
