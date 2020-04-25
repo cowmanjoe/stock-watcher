fcdf4c03cc46b8c7fb6c8bb53130397fe0a4fdc6
Joshua Yellowley <Joshuayellowley@gmail.com>
2020-04-25 00:45:40 -0700

    Added initialization of Sellers and Removal of seller commands

---
+++ backend/api/app/management/commands/initSellers.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand, CommandError
+from api.app.models import Seller as Seller
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('seller_names', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+
+
+        # Initialize Dummy Store Data
+        for seller_name in options["seller_names"]:
+            if(seller_name == "RANDOM"):
+                for i in range(10):
+                    store_name = 'store' + str(i)
+
+                    if(i % 2 == 0):
+                        location_name = 'near'
+                        stock_name = 'We got Hella'
+                    else:
+                        location_name = 'far'
+                        stock_name = 'We got None'
+                    
+                    seller = Seller(name=store_name, location=location_name, stock_levels=stock_name)
+                    seller.save()
+                print("Initialized Dummy Data")
+                break
+
+            # If not random, add the desired sellers        
+            seller = Seller(name=seller_name, location='Up your butt', stock_levels='We got Hella')
+            seller.save()
+
+            print("Added:", seller_name)
---
+++ backend/api/app/management/commands/removeSellers.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand, CommandError
+from api.app.models import Seller as Seller
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('seller_names', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        
+        # Iterate through inputted sellers
+        for seller_name in options['seller_names']:
+
+            # If inputted "ALL", delete all current sellers
+            if(seller_name == "ALL"):
+                Seller.objects.all().delete()
+                print("Removed all Sellers")
+                break
+            
+            # Try to remove the seller
+            try:
+                seller = Seller.objects.get(name=seller_name)
+            except Seller.DoesNotExist:
+                raise CommandError('Seller "%s" does not exist' %seller_name)
+            
+            # If multiple sellers with same name exist, delete all the sellers
+            except Seller.MultipleObjectsReturned:
+                sellers = Seller.objects.filter(name=seller_name)
+                sellers.delete()
+                print('Multiple Sellers with the name "%s" exist, All have been removed' %seller_name)
+
+            seller.opened = False
+            seller.save()
+
+            print("Removed:", seller_name)
+            
