84ae4ba363b21866bac490dc66615e128f0796be
Trevor Flanigan <trevorflanigan@alumni.ubc.ca>
2020-04-25 18:39:20 -0700

    added seller focus

---
+++ frontend/src/components/SellerFocus.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+export default class SellerFocus extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      seller: {},
+    };
+    this.renderProducts = this.renderProducts.bind(this);
+  }
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    console.log("IDD: " + id);
+
+    this.setState({ id: id });
+    let seller;
+    let response;
+    try {
+      response = await fetch(`http://localhost:8000/sellers/${id}`, {
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (err) {
+      console.log(err);
+      seller = {
+        name: "Error",
+        products: [
+          {
+            name: "Error",
+            stock: "Out of Stock",
+          },
+        ],
+      };
+    }
+
+    console.log(response);
+    let jsonResponse;
+
+    if (response) {
+      jsonResponse = await response.json();
+    }
+
+    console.log(jsonResponse);
+    if (!jsonResponse) {
+      seller = {};
+    } else {
+      seller = {
+        name: jsonResponse.name,
+        products: jsonResponse.products,
+        address: jsonResponse.address,
+        city: jsonResponse.city,
+        latitude: jsonResponse.latitude,
+        longitude: jsonResponse.longitude,
+        inventory_reports: jsonResponse.inventory_reports,
+      };
+    }
+
+    this.setState({
+      seller: seller,
+    });
+  }
+
+  renderProducts() {
+    let list = [];
+
+    if (this.state.seller.inventory_reports) {
+      this.state.seller.inventory_reports.forEach((report, i) => {
+        list.push(<li key={i}>{report.product.name + ": " + report.level}</li>);
+      });
+    }
+
+    return <ul style={{ listStyleType: "none" }}>{list}</ul>;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Title: {this.state.seller.name}</h1>
+        <h2>
+          Address: {this.state.seller.address}, {this.state.seller.city}
+        </h2>
+        <div>{this.renderProducts()}</div>
+      </div>
+    );
+  }
+}
